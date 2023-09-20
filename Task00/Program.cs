ae5c94fa3cdf85899cf6009239c5dc3eb5c90fc8
fenixfire1980 <djonmacane@yandex.ru>
2023-09-20 19:29:25 +0300

    Задача

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1. Программа на вход принимает число
+// 2. Выдает его в квадрате
+// 3. Вывод результата
+
+Console.WriteLine("Веедите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int square = number * number;
+//Console.WriteLine(number + " -> " + square);
+Console.WriteLine($"{number} -> {square}");
+
