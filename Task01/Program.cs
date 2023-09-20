5230fe9ff898a4810643a4b4cc68da7020f6cd4b
fenixfire1980 <djonmacane@yandex.ru>
2023-09-20 20:17:17 +0300

    Задача1

---
+++ Task01/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("Введите первое целое число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число:");
+
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number2 * number2 == number1)
+{
+    Console.WriteLine("Первое число является квадратом второго");
+}
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго");
+}
