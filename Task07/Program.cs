02e0ab462c0f474f710186e302f411973156e3a5
fenixfire1980 <djonmacane@yandex.ru>
2023-09-21 12:03:52 +0300

    Task7

--- Task07/Program.cs
+++ Task07/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+
+Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0) number *= -1;
+if (number >= 100 && number <= 999)
+{
+int LastDigit = number % 10;
+Console.WriteLine($"Последняя цифрв вашего числа {LastDigit}");
+}
+else 
+{
+    Console.WriteLine("Не коректный ввод");
+}
