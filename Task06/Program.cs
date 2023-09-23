22e4c7ecb47d4dbfa44fcde1ac844df7eee0c58d
fenixfire1980 <djonmacane@yandex.ru>
2023-09-23 15:49:39 +0300

    Task06 Homework

---
+++ Task06/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите целое число:");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
+{
+    Console.WriteLine("Число является четным");
+}
+else
+{
+    Console.WriteLine("Число является не четным");
+}
