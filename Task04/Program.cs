278119423450e0a621b27b359bf9da2d45bdec0e
fenixfire1980 <djonmacane@yandex.ru>
2023-09-23 15:40:45 +0300

    Task4 Homework

---
+++ Task04/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите первое целое число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите трейтее целое число:");
+int number3 = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+
+if (number2 > max) max = number2;
+if (number3 > max) max = number3;
+Console.WriteLine($"Большее число:{max}");
