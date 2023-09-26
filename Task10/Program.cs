9b95f3d61b5bb5ca21242be3ae1dd64bcd593b40
fenixfire1980 <djonmacane@yandex.ru>
2023-09-26 15:47:09 +0300

    Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int GetUserInput(string str)
+{
+    Console.Write($"{str}: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int SecondDigit(int num)
+{
+    return (num / 10) % 10;
+}
+
+int number = GetUserInput("Введите певрое число");
+
+if (number < 0) number *= -1;
+if (number >= 100 && number <= 999)
+{
+    int Second = SecondDigit(number);
+    Console.WriteLine($"Второе число {Second}");
+}
+else
+{
+    Console.WriteLine("Не коректный ввод");
+}
