b67aab3c4c7e2cdb6c2ab45a8cf79478ab3a2464
fenixfire1980 <djonmacane@yandex.ru>
2023-09-26 21:51:58 +0300

    Task18.1

--- Task18/Program.cs
+++ Task18/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+string RangeCoordenates(int number)
+{
+    if (number == 1) return "x > 0 && y > 0";
+    if (number == 2) return "x > 0 && y > 0";
+    if (number == 3) return "x > 0 && y > 0";
+    if (number == 4) return "x > 0 && y > 0";
+    return null;
+}
+
+Console.WriteLine("Введите номер четверти: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+string range = RangeCoordenates(num);
+
+string result = range != null
+        ? $"Диапазон координат для указаной четверти {range}"
+        : "Введен некоректный номер четверти";
+Console.WriteLine(result);
