8d0376ec52c69e3488ac77ce1c23f3e1bcb14c37
fenixfire1980 <djonmacane@yandex.ru>
2023-09-26 13:19:51 +0300

    Task10

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -1,11 +1,20 @@
-﻿
-
-int number = new Random().Next(10, 100);
+﻿int number =new Random().Next(10, 100);
 Console.WriteLine($"Случайное число из отрезка [10, 99] -> {number}");
-int FirstDigit = number / 10;
-int SecondDigit = number % 10;
-// if (FirstDigit > SecondDigit) Console.WriteLine($"Наибольшая цифра числа -> {FirstDigit}");
-// else Console.WriteLine($"Наибольшая цифра числа -> {SecondDigit}");
-// Тернарный оператор (тройной)
-int maxDigit = FirstDigit > SecondDigit ? FirstDigit : SecondDigit;
+
+// int firstDijit = number / 10;
+// int secondDigit = number % 10;
+
+// if (firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа -> {firstDigit}");
+// else Console.WriteLine($"Наибольшая цифра числа -> {secondDigit}");
+
+// int maxDigit = FirstDigit > secondDigit ? firstDigit : secondDigit; // Тернарный оператор
+// Console.WriteLine($"Наибольшая цифра числа -> {maxDigit}");
+
+int maxDigit = MaxDigit(number);
 Console.WriteLine($"Наибольшая цифра числа -> {maxDigit}");
+int MaxDigit (int num)
+{
+    int FirstDigit = num / 10;
+    int SecondDigit = num % 10;
+    return FirstDigit > SecondDigit ? FirstDigit : SecondDigit;
+}
---
+++ Task10/Program.cs
@@ -0,0 +1,7 @@
+﻿
+int number = new Random().Next(99, 1000);
+Console.WriteLine($"Случайное число из отрезка [99, 1000] -> {number}");
+int firstDigit = number / 100;
+int thirdDigit = number % 10;
+int result = firstDigit * 10 + thirdDigit;
+Console.WriteLine(result);
