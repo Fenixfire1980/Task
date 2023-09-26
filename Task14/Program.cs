14b8826daccef6af29209f3b97cae32221e0f9a4
fenixfire1980 <djonmacane@yandex.ru>
2023-09-26 14:43:30 +0300

    Task14

---
+++ Task14/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+int GetUserInput(string str)
+{
+    Console.Write($"{str}: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+bool DivisionTo7and23(int num)
+{
+    return num % 7 == 0 && num % 23 == 0;
+}
+
+
+int number = GetUserInput("Enter a number: ");
+bool result = DivisionTo7and23(number);
+
+
+Console.WriteLine(result ? $"{number} -> да" : $"{number} -> нет");
