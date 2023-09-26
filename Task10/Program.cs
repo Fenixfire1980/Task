1b62dd13314c84431d7abb4abaccddb25e905b8a
fenixfire1980 <djonmacane@yandex.ru>
2023-09-26 14:18:37 +0300

    Task12

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,7 +1,15 @@
 ﻿
 int number = new Random().Next(99, 1000);
 Console.WriteLine($"Случайное число из отрезка [99, 1000] -> {number}");
-int firstDigit = number / 100;
-int thirdDigit = number % 10;
-int result = firstDigit * 10 + thirdDigit;
+int DeletSecondDigit(int num)
+{
+    int firstDigit = num / 100;
+    int thirdDigit = num % 10;
+    return firstDigit * 10 + thirdDigit;
+}
+
+// int firstDigit = number / 100;
+// int thirdDigit = number % 10;
+// int result = firstDigit * 10 + thirdDigit;
+int result = (DeletSecondDigit(number));
 Console.WriteLine(result);
---
+++ Task12/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+
+
+
+int GetUserInput(string str)
+{
+    Console.Write($"{str}: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int GetRemainder(int num1, int num2)
+{
+   return num1 % num2;
+}
+
+
+
+int firstNum = GetUserInput("Введите первое число");
+int secondNum = GetUserInput("Введите второе число");
+
+int rem = GetRemainder(firstNum, secondNum);
+
+if (rem == 0)
+{
+    Console.WriteLine($"Число {firstNum} кратно {secondNum}");
+}
+else
+{
+     Console.WriteLine($"Число {firstNum} не кратно {secondNum} ,остаток {rem}");
+}
