46745325261860e64e1128d19ce4c33b8c9358ed
fenixfire1980 <djonmacane@yandex.ru>
2023-09-26 22:52:54 +0300

    Task22

---
+++ Task22/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+void SquareNumbers(int num)
+{
+    int count = 1;
+    while (count <= num)
+    {
+        Console.WriteLine($"{count, 3} -> {count * count, 5}");
+        count = count + 1;
+    }
+}
+
+Console.WriteLine("Enter digit");
+int number = Convert.ToInt32(Console.ReadLine());
+SquareNumbers(number);
