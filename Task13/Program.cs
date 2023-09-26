c366202739cb1f8eee99c5a2dd05e3820ef69333
fenixfire1980 <djonmacane@yandex.ru>
2023-09-26 16:57:44 +0300

    Task13HomeWork

---
+++ Task13/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+
+
+int GetUserInput(string str)
+{
+    Console.Write($"{str}: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int ThirdDigit(int num)
+{
+    int result = -1;
+    if (num >= 100)
+    {
+        while (num > 999)
+        {
+            num = num / 10;
+        }
+        result = num % 10;
+    }
+    return result;
+}
+
+int number = GetUserInput("Введите число");
+
+if (number < 0) number *= -1;
+if (ThirdDigit(number) == -1)
+{
+    Console.WriteLine("трейтей цифры нет");
+}
+else
+{
+    Console.WriteLine($"{number} -> {ThirdDigit(number)}");
+}
